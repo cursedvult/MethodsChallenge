1c171c2eb196deb861dd3a3332f31f82f5984680
Gustavo Miguel De Barros Carranço <GutMig1112@outlook.com>
2023-03-12 18:38:46 -0300

    Add project files.

---
+++ MethodsChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodsChallenge
+{
+    internal class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            string[] friends = {"Pedro" , "Jv", "Gabriel"};
+
+            for (int i = 0; i <= 2; i++) 
+            {
+                GreetFriend(friends[i]);
+            }
+        }
+        public static void GreetFriend(string amigo)
+        {
+            Console.WriteLine($"Hey there {amigo}, my dear friend!");
+        }
+    }
+}
